fec89ad23754ae63841ac94147f651e793f41ac8
sally-AH <sallyabdalhafeez@gmail.com>
2023-09-20 19:05:40 +0300

    Backend <> Added User API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -11,3 +11,32 @@ Route::group(["prefix" => "guest"], function(){
     Route::post("register", [AuthController::class, "register"]);
 });
 
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::group(["prefix" => "guest"], function(){
+    Route::get("unauthorized", [AuthController::class, "unauthorized"])->name("unauthorized");
+    Route::post("login", [AuthController::class, "login"]);
+    Route::post("register", [AuthController::class, "register"]);
+});
+
+Route::group(["middleware" => "auth:api"], function(){
+    Route::group(["prefix" => "user"], function(){
+        Route::post("logout", [AuthController::class, "logout"]);
+        Route::post("refresh", [AuthController::class, "refresh"]);
+    });
+});
