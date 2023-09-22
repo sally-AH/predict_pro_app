5d2ca584feb28de44e602946366cdd7b5c022c2c
sally-AH <sallyabdalhafeez@gmail.com>
2023-09-22 23:42:52 +0300

    Frontend <> Added Info Card Widget

--- frontend/lib/components/side_menu.dart
+++ frontend/lib/components/side_menu.dart
@@ -64,3 +64,33 @@ class _SideMenuState extends State<SideMenu> {
     );
   }
 }
+
+class InfoCard extends StatelessWidget {
+  const InfoCard({
+    Key? key,
+    required this.name,
+    required this.position,
+  }) : super(key: key);
+  final String name, position;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: const CircleAvatar(
+        backgroundColor: Colors.white24,
+        child: Icon(
+          CupertinoIcons.person,
+          color: Colors.white,
+        ),
+      ),
+      title: Text(
+        name,
+        style: TextStyle(color: Colors.white),
+      ),
+      subtitle: Text(
+        position,
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+}
