568a2e0bcd9bbc4ea4eaaa3391f399c64cd874b4
sally-AH <sallyabdalhafeez@gmail.com>
2023-09-22 23:38:02 +0300

    Frontend <> Added Dashboard GUI

--- frontend/lib/screens/dashboard.dart
+++ frontend/lib/screens/dashboard.dart
@@ -11,6 +11,14 @@ class _DashboardState extends State<Dashboard> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      extendBody: true,
+      drawer: SideMenu(),
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(0, 149, 255, 100),
+        title: Text("Dashboard"),
+      ),
+    );
   }
 }
