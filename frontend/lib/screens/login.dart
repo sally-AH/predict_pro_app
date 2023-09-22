e49367c5350953f199228b7025e8f2a3d6ea2b5c
sally-AH <sallyabdalhafeez@gmail.com>
2023-09-22 21:06:50 +0300

    Frontend <> Added Login GUI

--- frontend/lib/screens/login.dart
+++ frontend/lib/screens/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/screens/singup.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -10,6 +11,160 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return GestureDetector(
+      onTap: ()=> FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            onPressed: (){
+              Navigator.pop(context);
+            },
+            icon: Icon(
+              Icons.arrow_back,
+              size: 20,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Text(
+                          "Login",
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 20,),
+                        Text(
+                          "Login to your account",
+                          style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.grey[700]
+                          ),
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+                      child: Form(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            TextFormField(
+                              validator: (value){
+                                if(value!=null){
+                                  if(value.isEmpty || !value.contains('@')){
+                                    return 'Please Enter a valid email.';
+                                  }
+                                }
+                                return null;
+                              },
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: InputDecoration(
+                                  labelText: 'Email Address'
+                              ),
+                            ),
+                            TextFormField(
+                              validator: (value){
+                                if(value!=null){
+                                  if(value.isEmpty || value!.length < 7){
+                                    return 'Password must be at leat 7 characters.';
+                                  }
+                                }
+                                return null;
+                              },
+                              decoration: InputDecoration(
+                                  labelText: 'Password'
+                              ),
+                              obscureText: true,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+                      child: Container(
+                        padding: EdgeInsets.only(top: 30, left: 3),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: MaterialButton(
+                          minWidth: double.infinity,
+                          height: 60,
+                          onPressed: (){
+                            
+                          },
+                          color: Color(0xff0095FF),
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                                color: Colors.black
+                            ),
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          child: Text(
+                            "Login",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 18,
+                                color: Colors.white
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Don't have an account?",
+                        ),
+                        TextButton(
+                            child: Text(
+                              "sign up",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 18,
+                              ),
+                            ),
+                            onPressed: (){
+                              Navigator.push(context, MaterialPageRoute(builder: (context)=> SignUpPage()));
+                            }
+                        )
+
+                      ],
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(top: 100),
+                      height: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("assets/background.png")
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
