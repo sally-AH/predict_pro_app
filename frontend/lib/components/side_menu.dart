cc95f2d739c9cbc8431d0fe774c273d7e1cd2628
sally-AH <sallyabdalhafeez@gmail.com>
2023-09-22 23:43:44 +0300

    Frontend <> Added Side Menu Item Widget

--- frontend/lib/components/side_menu.dart
+++ frontend/lib/components/side_menu.dart
@@ -64,6 +64,65 @@ class _SideMenuState extends State<SideMenu> {
     );
   }
 }
+class SideMenuItems extends StatelessWidget {
+  const SideMenuItems({
+    Key? key,
+    required this.menu,
+    required this.press,
+    required this.riveonInit,
+    required this.isActive,
+  }) : super(key: key);
+
+  final RiveAsset menu;
+  final VoidCallback press;
+  final ValueChanged<Artboard> riveonInit;
+  final bool isActive;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Padding(
+          padding: EdgeInsets.only(left: 24),
+          child: Divider(
+            color: Colors.white24,
+            height: 1,
+          ),
+        ),
+        Stack(children: [
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.fastOutSlowIn,
+            height: 56,
+            width: isActive ? 288 : 0,
+            left: 0,
+            child: Container(
+              decoration: const BoxDecoration(
+                  color: Color.fromRGBO(0, 149, 255, 100),
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+            ),
+          ),
+          ListTile(
+            onTap: press,
+            leading: SizedBox(
+              height: 34,
+              width: 34,
+              child: RiveAnimation.asset(
+                menu.src,
+                artboard: menu.artboard,
+                onInit: riveonInit,
+              ),
+            ),
+            title: Text(
+              menu.title,
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ]),
+      ],
+    );
+  }
+}
 
 class InfoCard extends StatelessWidget {
   const InfoCard({
