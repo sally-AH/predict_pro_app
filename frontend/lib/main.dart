34db3b83f5773a47ef52febb9347b9750d41d48c
sally-AH <sallyabdalhafeez@gmail.com>
2023-09-24 08:37:31 +0300

    Frontend <> Updated Notification

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -11,10 +11,32 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:predict_pro/firebase_options.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
 
 
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  print("Token test");
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  print("Token test1");
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print("Token $fcmToken");
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
 
 
   runApp(
