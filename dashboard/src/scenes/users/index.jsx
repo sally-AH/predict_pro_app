2d474f1039def8e365ebd293150dda858a34b006
sally-AH <sallyabdalhafeez@gmail.com>
2023-10-01 21:38:46 +0300

    Frontend <> Imported Needed Packages

--- dashboard/src/scenes/users/index.jsx
+++ dashboard/src/scenes/users/index.jsx
@@ -1,7 +1,14 @@
 
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+
 
 const User = ()=>{
     return <div>User</div>
 }
 
-export default User
+export default User 
