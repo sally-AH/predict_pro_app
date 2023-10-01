361e25904ccfca9f875d986df23d233f602bcb7a
sally-AH <sallyabdalhafeez@gmail.com>
2023-10-01 22:38:20 +0300

    Frontend <> Added Users Page

---
+++ dashboard/src/data/mockData.js
@@ -0,0 +1,28 @@
+import { tokens } from "../theme";
+
+export const mockDataTeam = [
+  {
+    "id": 1,
+    "usertype_id": 1,
+    "branch_id": 1,
+    "username": "khalil",
+    "email": "km@sef.com\n",
+    "phone": "70777888999",
+    "pic_url": "https://picsum.photos/200",
+    "is_active": 1,
+    "created_at": "2023-09-24T18:44:25.000000Z",
+    "updated_at": "2023-09-24T18:44:25.000000Z"
+  },
+  {
+    "id": 2,
+    "usertype_id": 1,
+    "branch_id": 1,
+    "username": "sally",
+    "email": "sally@sef.com",
+    "phone": "70777888999",
+    "pic_url": "https://picsum.photos/200",
+    "is_active": 1,
+    "created_at": "2023-09-29T03:06:57.000000Z",
+    "updated_at": "2023-09-29T03:06:57.000000Z"
+  }
+];
--- dashboard/src/scenes/users/index.jsx
+++ dashboard/src/scenes/users/index.jsx
@@ -1,4 +1,3 @@
-
 import { Box, Typography, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
@@ -6,9 +5,65 @@ import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettin
 import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
 import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
 
+import { getUsers } from "../../data/users";
 
-const User = ()=>{
-    return <div>User</div>
-}
+const User = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "username",
+      headerName: "Username",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+  ];
 
-export default User 
+  return (
+    <Box m="20px">
+      {/* <Header title="TEAM" subtitle="Managing the Team Members" /> */}
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.green[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blue[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.black[400],
+          },
+
+          "& .MuiCheckbox-root": {
+            color: `${colors.green[200]} !important`,
+          },
+        }}
+      >
+
+        <DataGrid checkboxSelection rows={mockDataTeam} columns={columns} />
+      </Box>
+    </Box>
+  );
+};
+
+export default User;
