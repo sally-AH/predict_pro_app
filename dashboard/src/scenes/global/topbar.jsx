90c6a89cac12c5130e718a3a5aad161021c40b24
sally-AH <sallyabdalhafeez@gmail.com>
2023-10-01 19:50:07 +0300

    Frontend <> Added Topbar Icons

--- dashboard/src/scenes/global/topbar.jsx
+++ dashboard/src/scenes/global/topbar.jsx
@@ -20,12 +20,35 @@ const Topbar = ()=>{
             <Box display="flex" backgroundColor={colors.black[400]} borderRadius="3px">
                 <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
                 <IconButton type="button" sx={{ p: 1 }}>
-                <SearchIcon />
+                    <SearchIcon />
                 </IconButton>
             </Box>
-            <IconButton></IconButton>
+
+            {/* ICONS */}
+            <Box display="flex">
+                <IconButton onClick={colorMode.toggleColorMode}>
+                    {theme.palette.mode === "dark" ? (
+                        <DarkModeOutlinedIcon />
+                    ) : (
+                        <LightModeOutlinedIcon />
+                    )}
+                </IconButton>
+                <IconButton>
+                    <NotificationsOutlinedIcon />
+                </IconButton>
+                <IconButton>
+                    <SettingsOutlinedIcon />
+                </IconButton>
+                <IconButton>
+                    <PersonOutlinedIcon />
+                </IconButton>
+            </Box>
         </Box>
-    )
-}
+
+
+        
+        
+    );
+};
 
 export default Topbar
