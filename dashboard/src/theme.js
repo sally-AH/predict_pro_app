1aa44a4a177a8a4e4a225bb91f866943704b9b8a
sally-AH <sallyabdalhafeez@gmail.com>
2023-10-01 14:45:30 +0300

    Frontend <> Created Use Mode Function

--- dashboard/src/theme.js
+++ dashboard/src/theme.js
@@ -162,6 +162,20 @@ export const themeSettings = (mode) => {
 
 export const ColorModeContext = createContext({
     toggleColorMode: ()=>{
-        
+
     }
 })
+
+export const useMode = () => {
+    const [mode, setMode] = useState("dark");
+  
+    const colorMode = useMemo(
+      () => ({
+        toggleColorMode: () =>
+          setMode((prev) => (prev === "light" ? "dark" : "light")),
+      }),
+      []
+    );
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+    return [theme, colorMode];
+};
