ca56a858f83ac8259062d2749f8830a1b3b11f96
sally-AH <sallyabdalhafeez@gmail.com>
2023-10-01 20:02:01 +0300

    Frontend <> Added Sidebar to App FIle

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -2,7 +2,7 @@ import {ColorModeContext, useMode} from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import Topbar from './scenes/global/topbar';
-
+import Sidebar  from './scenes/global/sidebar';
 function App() {
   const [theme, colorMode] = useMode();
 
@@ -11,10 +11,11 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline/>
         <div className="App">
+          <Sidebar/>
           <main className='content'>
             <Topbar/>
             <Routes>
-              <Route path='/' element={}></Route>
+              <Route path='/'></Route>
             </Routes>
           </main>
         </div>
