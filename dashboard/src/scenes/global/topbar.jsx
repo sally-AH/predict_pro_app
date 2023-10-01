8768284aea63e8e3fc83faed96dce1d6cb456228
sally-AH <sallyabdalhafeez@gmail.com>
2023-10-01 19:41:41 +0300

    Frontend <> Added Search bar

--- dashboard/src/scenes/global/topbar.jsx
+++ dashboard/src/scenes/global/topbar.jsx
@@ -1,7 +1,13 @@
 import { Box, IconButton, useTheme } from "@mui/material";
 import { useContext } from "react";
 import { ColorModeContext, tokens } from "../../theme";
-
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Topbar = ()=>{
     const theme = useTheme();
@@ -9,7 +15,16 @@ const Topbar = ()=>{
     const colorMode = useContext(ColorModeContext);
 
     return (
-  
+        <Box display="flex" justifyContent="space-between" p={2}>
+            {/* Search Bar */}
+            <Box display="flex" backgroundColor={colors.black[400]} borderRadius="3px">
+                <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+                <IconButton type="button" sx={{ p: 1 }}>
+                <SearchIcon />
+                </IconButton>
+            </Box>
+            <IconButton></IconButton>
+        </Box>
     )
 }
 
